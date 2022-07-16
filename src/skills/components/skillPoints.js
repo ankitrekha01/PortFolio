94f8c73f72ab06c85d3449bf9e0f85f9d38f0d70
ankitrekha01 <ankitrekha27@gmail.com>
2022-07-16 10:36:23 +0530

    change in skills arr

--- src/skills/components/skillPoints.js
+++ src/skills/components/skillPoints.js
@@ -1,5 +1,5 @@
 const skillPoints = [
-    `Develop highly interactive Front end / User Interfaces for your web and mobile applications`,
+    `Develop highly interactive Front end / User Interfaces for web and mobile applications`,
     `Building responsive website front end using ReactJS`,
     `Developing mobile applications using React-Native`,
     `Creating application backend in Node & Express, integrating database using MongoDB & MySQL`
